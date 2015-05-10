0a33029060fc8aedbd368eb3a83ecbbb7a4cb7cc
Shun <theodore.gao.1986@gmail.com>
2015-05-09 17:39:28 -0700

    Build MainMenu framework for incoming functions.

--- app/src/main/java/com/jack/android/tutorial/MainActivity.java
+++ app/src/main/java/com/jack/android/tutorial/MainActivity.java
@@ -1,39 +1,42 @@
 package com.jack.android.tutorial;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final Class<?>[] mActivities = new Class<?>[]{
+            CalculatorActivity.class
+    };
+    AdapterView.OnItemClickListener mMainMenuOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            startActivity(new Intent(MainActivity.this, mActivities[i]));
+        }
+    };
+    private ListView mListView;
+    private ArrayAdapter<String> mMainMenuAdapter;
+    private String[] mMainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mMainMenu = getResources().getStringArray(R.array.main_menu);
+        mMainMenuAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mMainMenu);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mListView = (ListView) findViewById(R.id.listMainMenu);
+        mListView.setAdapter(mMainMenuAdapter);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mListView.setOnItemClickListener(mMainMenuOnItemClickListener);
     }
 }
